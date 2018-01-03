e31bb1b8dc2605855f53e65e17088268479c8af8
JT <jt@techbytes.io>
2018-01-03 01:02:37 -0500

    Twitter Reply Bot

---
+++ README.md
@@ -0,0 +1,16 @@
+# Twitter BOT README (Reply bot for @mentions)
+
+Written in nodeJS
+
+### Bots inculded in this program
+
+* Reply Bot
+	+ Runs constant
+
+### Get it running
+Add your Twitter API creds to your .env file
+
+Written by techBytesIO
+Twitter: [Follow me @techBytesIO](https://www.twitter.com/techbytesIO)
+Blog: [Check me out!](https://techbytes.io)
+
---
+++ bot.js
@@ -0,0 +1,45 @@
+// REPLYING BOT====================
+// Dependencies =========================
+var
+    twit = require('twit');
+    config = require('./config');
+
+var Twitter = new twit(config);
+
+// Set up User Stream
+console.log('Launching the replying bot...' + '\n');
+
+var stream = Twitter.stream('user');
+
+console.log('Set up the user stream' + '\n');
+
+console.log('Listening and waiting...' + '\n');
+
+stream.on('tweet', (t) => {
+	var replyingto = t.in_reply_to_screen_name;
+	var text = t.text;
+	var from = t.user.screen_name;
+	console.log(from + ' sent ' + text + '\n');
+
+  	if (replyingto == '4TunGaming') {
+		var newTweet = '@' + from + '-- Game ON!! #indie #dev #gaming';
+		tweetIt(newTweet);
+	}
+});
+
+function tweetIt(txt) {
+	var tweet = {
+		status: txt 
+	}
+
+	Twitter.post('statuses/update', tweet, tweeted);
+
+	function tweeted(err, data, res) {
+		if (err) {
+			console.log("Something went wrong!! You'll have to fix it! \n");
+		} else {
+			console.log("Tweet replied to!!! \n");
+		}
+	}
+	
+};
